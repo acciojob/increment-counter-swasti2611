58c020500fc1bb3853ab275d47ae7810d76bfe9c
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-13 12:59:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+
+let counter=0;
+const counterValue = document.getElementById('counter');
+const incrementBtn = document.getElementById('incrementBtn');
+
+ 
+// To increment the value of counter
+incrementBtn.addEventListener('click', () => {
+    counter++;
+    counterValue.innerHTML = counter;
+	alert(`${counter}`)
+});
