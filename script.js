e1c1553dd31c3c88866993a3c6e47765acd76502
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-19 11:14:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,17 +3,13 @@ let counter=0;
 const counterValue = document.getElementById('counter');
 const incrementBtn = document.getElementById('incrementBtn');
 
- function counter(){
-	 counter++;
- }
+ 
 // To increment the value of counter
 incrementBtn.addEventListener('click', () => {
     counter=counter+1;
 	
-	 
+	
 	counterValue.innerHTML = counter;
-
-	alert(`${counter}`);
-    
+    alert(`${counter}`);
 	
 });
