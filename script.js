8e9cf9d12a107f3b07454c4561ae66d82c73ca39
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-19 11:53:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 
-let counter=0;
-const counterValue = document.getElementById('counter');
-const incrementBtn = document.getElementById('incrementBtn');
+function increment() {
+        let count = parseInt(document.getElementById("counter").innerHTML);
+        alert(count);
+        count++;
+        counter.innerHTML = count;
+    }
+// const counterValue = document.getElementById('counter');
+// const incrementBtn = document.getElementById('incrementBtn');
 
- 
-// To increment the value of counter
-incrementBtn.addEventListener('click', (event) => {
-	event.preventDefault()
-	 alert(`${counter}`);
-    counter=counter+1;
+//  let counter=0;
+// // To increment the value of counter
+// incrementBtn.addEventListener('click', (event) => {
+// 	event.preventDefault()
+// 	 alert(`${counter}`);
+//     counter=counter+1;
 	
 	
-	counterValue.innerHTML = counter;
+// 	counterValue.innerHTML = counter;
    
 	
-});
+// });
